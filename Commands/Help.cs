e80e4ed5bded70a710149cdcbae09706ad2f4027
lucas DELOLME <lucas.delolme@orange.fr>
2021-01-16 18:36:19 +0100

    [revert]

--- Commands/Help.cs
+++ Commands/Help.cs
@@ -14,6 +14,7 @@ namespace Valhallapp.Modules
         {
             DisplayCommandLine("help", Context.Message.Author.Username, Context.Channel.Name);
             var embed = new EmbedBuilder();
+            string prefix = "&";
             embed.WithTitle("Commands list:")
                 .WithAuthor(Context.Client.CurrentUser)
                 .AddField($"{prefix}femboy", "Display the seeded femboy percentage rating of the a user. Can accept one paramater.")
@@ -22,7 +23,7 @@ namespace Valhallapp.Modules
                 .AddField($"{prefix}heroku", "Display the app hosting site of the [application](https://dashboard.heroku.com/apps/valhallapp)!")
                 .AddField($"{prefix}github", "Display the github repo of the [application](https://github.com/dryadt/valhallapp)!")
                 .AddField($"{prefix}help", "Displays help related to the bot!")
-                .AddField($"{prefix}ping", "Replies with the ping of the bot.")
+                .AddField($"{prefix}ping", "Replies with the ping of the bot")
                 .AddField($"{prefix}website", "Display the website of the [application](https://valhallapp.herokuapp.com/)!")
                 .WithFooter(footer => footer.Text = "Page 1 out of 1.")
                 .WithColor(Color.Blue)
@@ -36,12 +37,13 @@ namespace Valhallapp.Modules
         {
             DisplayCommandLine("Help meme", Context.Message.Author.Username, Context.Channel.Name);
             var embed = new EmbedBuilder();
+            string prefix = "&";
             embed.WithTitle("Meme commands list:")
                 .WithAuthor(Context.Client.CurrentUser)
                 .AddField($"{prefix}astral", "A.")
                 .AddField($"{prefix}costro", "Gay stag.")
-                .AddField($"{prefix}hi", "He likes guys.")
-                .AddField($"{prefix}dikidikidikidesu ka ?\n" + $"{prefix}Oh! You go big guy!\n" +  $"{prefix}Pose for the fans!\n" + $"{prefix}Wubbadubadub is that true?", "Do I need to say anything about the GOTY?")
+                .AddField($"{prefix}hi", "He likes guys")
+                .AddField($"{prefix}Oh! You go big guy!\n" + $"{prefix}Pose for the fans!\n" + $"{prefix}Wubbadubadub is that true?", "Do i need to say anything about the GOTY?")
                 .AddField($"{prefix}starless", "Mega Bi stag.")
                 .WithFooter(footer => footer.Text = "Page 1 out of 1.")
                 .WithColor(Color.Blue)
